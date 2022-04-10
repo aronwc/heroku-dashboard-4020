f071bf8e364885918d630e835110e4bffb3322d8
Victoria <victoria4142@gmail.com>
2022-04-10 18:24:11 -0500

    changes???

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,7 +117,6 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default']=(db_from_env)
-#DATABASES['default'] = dj_database_url.config(default=os.getenv('whoami')) 
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
