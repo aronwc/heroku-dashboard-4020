366dcda98573c45d086783dc04b17dad39701c2d
Victoria <victoria4142@gmail.com>
2022-04-10 17:26:40 -0500

    heroku change

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'heroku-dashboard-4020.herokuapp.com']
 
 
 # Application definition
